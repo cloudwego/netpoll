04cd4947d512767e9871c88e18a93fc7f7ab89cf
Jacob953 <jacob953@csu.edu.cn>
2022-08-30 02:33:29 +0800

    fix: uniform variable r to u

--- uring/sys_mmap.go
+++ uring/sys_mmap.go
@@ -20,82 +20,82 @@ import (
 )
 
 // sysMmap is used to free the URingSQE and URingCQE,
-func (r *URing) sysMunmap() (err error) {
-	err = syscall.Munmap(r.sqRing.buff)
-	if r.cqRing.buff != nil && &r.cqRing.buff[0] != &r.sqRing.buff[0] {
-		err = syscall.Munmap(r.cqRing.buff)
+func (u *URing) sysMunmap() (err error) {
+	err = syscall.Munmap(u.sqRing.buff)
+	if u.cqRing.buff != nil && &u.cqRing.buff[0] != &u.sqRing.buff[0] {
+		err = syscall.Munmap(u.cqRing.buff)
 	}
 	return
 }
 
 // sysMmap is used to configure the URingSQE and URingCQE,
 // it should only be called after the sysSetUp function has completed successfully.
-func (r *URing) sysMmap(p *ringParams) (err error) {
+func (u *URing) sysMmap(p *ringParams) (err error) {
 	size := unsafe.Sizeof(URingCQE{})
 	if p.flags&IORING_SETUP_CQE32 != 0 {
 		size += unsafe.Sizeof(URingCQE{})
 	}
-	r.sqRing.ringSize = uint64(p.sqOffset.array) + uint64(p.sqEntries*(uint32)(unsafe.Sizeof(uint32(0))))
-	r.cqRing.ringSize = uint64(p.cqOffset.cqes) + uint64(p.cqEntries*(uint32)(size))
+	u.sqRing.ringSize = uint64(p.sqOffset.array) + uint64(p.sqEntries*(uint32)(unsafe.Sizeof(uint32(0))))
+	u.cqRing.ringSize = uint64(p.cqOffset.cqes) + uint64(p.cqEntries*(uint32)(size))
 
 	if p.features&IORING_FEAT_SINGLE_MMAP != 0 {
-		if r.cqRing.ringSize > r.sqRing.ringSize {
-			r.sqRing.ringSize = r.cqRing.ringSize
+		if u.cqRing.ringSize > u.sqRing.ringSize {
+			u.sqRing.ringSize = u.cqRing.ringSize
 		}
-		r.cqRing.ringSize = r.sqRing.ringSize
+		u.cqRing.ringSize = u.sqRing.ringSize
 	}
 
 	// TODO: syscall.MAP_POPULATE unsupport for macox
-	data, err := syscall.Mmap(r.fd, 0, int(r.sqRing.ringSize), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	data, err := syscall.Mmap(u.fd, 0, int(u.sqRing.ringSize), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
 	if err != nil {
 		return err
 	}
-	r.sqRing.buff = data
+	u.sqRing.buff = data
 
 	if p.features&IORING_FEAT_SINGLE_MMAP != 0 {
-		r.cqRing.buff = r.sqRing.buff
+		u.cqRing.buff = u.sqRing.buff
 	} else {
 		// TODO: syscall.MAP_POPULATE unsupport for macox
-		data, err = syscall.Mmap(r.fd, int64(IORING_OFF_CQ_RING), int(r.cqRing.ringSize), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+		data, err = syscall.Mmap(u.fd, int64(IORING_OFF_CQ_RING), int(u.cqRing.ringSize), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
 		if err != nil {
-			r.sysMunmap()
+			u.sysMunmap()
 			return err
 		}
-		r.cqRing.buff = data
+		u.cqRing.buff = data
 	}
 
-	ringStart := &r.sqRing.buff[0]
-	r.sqRing.kHead = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.head)))
-	r.sqRing.kTail = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.tail)))
-	r.sqRing.kRingMask = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.ringMask)))
-	r.sqRing.kRingEntries = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.ringEntries)))
-	r.sqRing.kFlags = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.flags)))
-	r.sqRing.kDropped = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.dropped)))
-	r.sqRing.array = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.array)))
+	ringStart := &u.sqRing.buff[0]
+	u.sqRing.kHead = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.head)))
+	u.sqRing.kTail = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.tail)))
+	u.sqRing.kRingMask = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.ringMask)))
+	u.sqRing.kRingEntries = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.ringEntries)))
+	u.sqRing.kFlags = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.flags)))
+	u.sqRing.kDropped = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.dropped)))
+	u.sqRing.array = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.sqOffset.array)))
 
 	size = unsafe.Sizeof(URingSQE{})
 	if p.flags&IORING_SETUP_SQE128 != 0 {
 		size += 64
 	}
 	// TODO: syscall.MAP_POPULATE unsupport for macox
-	buff, err := syscall.Mmap(r.fd, int64(IORING_OFF_SQES), int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	buff, err := syscall.Mmap(u.fd, int64(IORING_OFF_SQES), int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
 	if err != nil {
-		_ = r.sysMunmap()
+		_ = u.sysMunmap()
 		return err
 	}
-	r.sqRing.sqeBuff = buff
+	u.sqRing.sqeBuff = buff
 
-	cqRingPtr := uintptr(unsafe.Pointer(&r.cqRing.buff[0]))
-	ringStart = &r.cqRing.buff[0]
+	cqRingPtr := uintptr(unsafe.Pointer(&u.cqRing.buff[0]))
+	ringStart = &u.cqRing.buff[0]
 
-	r.cqRing.kHead = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.head)))
-	r.cqRing.kTail = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.tail)))
-	r.cqRing.kRingMask = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.ringMsk)))
-	r.cqRing.kRingEntries = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.ringEntries)))
-	r.cqRing.kOverflow = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.overflow)))
-	r.cqRing.cqes = (*URingCQE)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.cqes)))
+	u.cqRing.kHead = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.head)))
+	u.cqRing.kTail = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.tail)))
+	u.cqRing.kRingMask = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.ringMsk)))
+	u.cqRing.kRingEntries = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.ringEntries)))
+	u.cqRing.kOverflow = (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.overflow)))
+	u.cqRing.cqes = (*URingCQE)(unsafe.Pointer(uintptr(unsafe.Pointer(ringStart)) + uintptr(p.cqOffset.cqes)))
 	if p.cqOffset.flags != 0 {
-		r.cqRing.kFlags = cqRingPtr + uintptr(p.cqOffset.flags)
+		u.cqRing.kFlags = cqRingPtr + uintptr(p.cqOffset.flags)
 	}
 
 	return nil
