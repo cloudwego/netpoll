13a4b2b469f17cc2eb7e64f8405d1f06b8e1aa8d
wangzhuowei <wangzhuowei@bytedance.com>
2022-10-18 14:32:50 +0800

    perf: use fixed arena

--- pcache.go
+++ pcache.go
@@ -1,8 +1,11 @@
 package netpoll
 
 import (
+	"log"
 	"math/bits"
+	"reflect"
 	"runtime"
+	"unsafe"
 	_ "unsafe"
 )
 
@@ -23,17 +26,21 @@ func Free(buf []byte) {
 }
 
 const (
-	defaultPCacheMaxSize     = 42               // 2^42=4 TB
-	defaultPCacheLimitPerP   = 1024 * 1024 * 64 // 64 MB
-	defaultPCacheCleanCycles = 3                // clean inactive every 3 gc cycles
+	debug                          = false
+	defaultPCacheMaxSize           = 42 // 2^42=4 TB
+	defaultPCacheActiveLimitPerP   = 1024 * 1024 * 32
+	defaultPCacheInactiveLimitPerP = 1024 * 1024 * 16
+	defaultPCacheCleanCycles       = 3
 )
 
 type pcache struct {
-	active      [][defaultPCacheMaxSize][][]byte // [pid][cap_idx][idx][]byte
-	activeSize  []int64                          // [pid]int64, every P's active size
-	activeLimit int64                            // active_limit_size = total_limit / GOMAXPROCS
-	inactive    [][defaultPCacheMaxSize][][]byte // [pid][cap_idx][idx][]byte
-	ref         *pcacheRef
+	arena      []byte
+	arenaStart uintptr                          // arena address start
+	arenaEnd   uintptr                          // arena address end
+	blocks     [][]byte                         // [pid][]byte, pid=(addr-addr_start)/block_size
+	active     [][defaultPCacheMaxSize][][]byte // [pid][cap_idx][idx]stack
+	inactive   [][defaultPCacheMaxSize][][]byte // [pid][cap_idx][idx]stack
+	ref        *pcacheRef
 }
 
 type pcacheRef struct {
@@ -42,36 +49,63 @@ type pcacheRef struct {
 }
 
 func gcRefHandler(ref *pcacheRef) {
+	defer runtime.SetFinalizer(ref, gcRefHandler)
 	ref.gc++
-	// trigger handler every gc cycle
-	if ref.gc >= defaultPCacheCleanCycles {
-		ref.gc = 0
-		pid := procPin()
-		var l int
-		for i := 0; i < defaultPCacheMaxSize; i++ {
-			l = len(ref.pc.inactive[pid][i])
-			if l == 0 {
-				continue
-			}
-			ref.pc.inactive[pid][i] = ref.pc.inactive[pid][i][:l/2]
-		}
-		procUnpin()
+	if ref.gc < defaultPCacheCleanCycles {
+		return
 	}
-	runtime.SetFinalizer(ref, gcRefHandler)
+	ref.gc = 0
+
+	// trigger handler
+	pid := procPin()
+	var cached int
+	for i := 0; i < defaultPCacheMaxSize; i++ {
+		cached += (1 << i) * len(ref.pc.inactive[pid][i])
+	}
+	if cached == 0 || cached < defaultPCacheInactiveLimitPerP {
+		procUnpin()
+		return
+	}
+
+	var buf [][]byte
+	var l, c, released int
+	for i := 0; i < defaultPCacheMaxSize; i++ {
+		l = len(ref.pc.inactive[pid][i])
+		if l == 0 {
+			continue
+		}
+		c = 1 << i
+		buf = make([][]byte, l/2, l)
+		copy(buf, ref.pc.inactive[pid][i][:l/2]) // the first l/2 items are more inactive
+		ref.pc.inactive[pid][i] = buf
+		released += c * len(buf)
+	}
+	procUnpin()
+	if debug && released > 0 {
+		log.Printf("PCACHE: P[%d] release: %d bytes", pid, released)
+	}
 }
 
 func newPCache() *pcache {
-	return newLimitedPCache(defaultPCacheLimitPerP)
+	return newLimitedPCache(defaultPCacheActiveLimitPerP)
 }
 
-func newLimitedPCache(limitPerP int64) *pcache {
+func newLimitedPCache(limitPerP int) *pcache {
 	procs := runtime.GOMAXPROCS(0)
 	pc := &pcache{
-		active:      make([][defaultPCacheMaxSize][][]byte, procs),
-		activeSize:  make([]int64, procs),
-		inactive:    make([][defaultPCacheMaxSize][][]byte, procs),
-		activeLimit: limitPerP,
+		blocks:   make([][]byte, procs),
+		active:   make([][defaultPCacheMaxSize][][]byte, procs),
+		inactive: make([][defaultPCacheMaxSize][][]byte, procs),
 	}
+
+	// init arena
+	pc.arena = NewArena(limitPerP * procs)
+	pc.arenaStart = uintptr(unsafe.Pointer(&pc.arena[0]))
+	pc.arenaEnd = uintptr(unsafe.Pointer(&pc.arena[len(pc.arena)-1]))
+	for i := 0; i < procs; i++ {
+		pc.blocks[i] = pc.arena[i*limitPerP : (i+1)*limitPerP]
+	}
+
 	pc.ref = &pcacheRef{pc: pc}
 	runtime.SetFinalizer(pc.ref, gcRefHandler)
 	pc.ref = nil // trigger gc
@@ -84,37 +118,77 @@ func (p *pcache) Malloc(size int, _capacity ...int) (data []byte) {
 		capacity = _capacity[0]
 	}
 	cidx := calcCapIndex(capacity)
-	capacity = 1 << cidx
+	clen := 1 << cidx
 
 	pid := procPin()
-	if len(p.active[pid][cidx]) > 0 {
-		data = p.active[pid][cidx][0]
-		p.active[pid][cidx] = p.active[pid][cidx][1:]
-		p.activeSize[pid] -= int64(capacity)
-	} else if len(p.inactive[pid][cidx]) > 0 {
-		data = p.inactive[pid][cidx][0]
-		p.inactive[pid][cidx] = p.inactive[pid][cidx][1:]
-	} else {
-		data = make([]byte, size, capacity)
+	l := len(p.active[pid][cidx])
+	if l > 0 {
+		data = p.active[pid][cidx][l-1][:size:capacity]
+		p.active[pid][cidx] = p.active[pid][cidx][:l-1]
+		procUnpin()
+		if debug {
+			log.Printf("PCACHE: P[%d] reuse active %d bytes, addr %d", pid, clen, uintptr(unsafe.Pointer(&data[:1][0])))
+		}
+		return data
 	}
+
+	l = len(p.inactive[pid][cidx])
+	if l > 0 {
+		data = p.inactive[pid][cidx][l-1][:size:capacity]
+		p.inactive[pid][cidx] = p.inactive[pid][cidx][:l-1]
+		procUnpin()
+		if debug {
+			log.Printf("PCACHE: P[%d] reuse inactive %d bytes, addr %d", pid, clen, uintptr(unsafe.Pointer(&data[:1][0])))
+		}
+		return data
+	}
+
+	if clen <= len(p.blocks[pid]) {
+		data = p.blocks[pid][:size:capacity]
+		p.blocks[pid] = p.blocks[pid][clen:] // need occupy full clen not only capacity
+		procUnpin()
+		if debug {
+			log.Printf("PCACHE: P[%d] reuse arena %d bytes, addr %d", pid, clen, uintptr(unsafe.Pointer(&data[:1][0])))
+		}
+		return data
+	}
+
 	procUnpin()
-	return data[:size]
+	// malloc full clen buffer but only use capacity size
+	data = make([]byte, size, clen)[:size:capacity]
+	if debug {
+		log.Printf("PCACHE: P[%d] malloc %d bytes, addr %d", pid, clen, uintptr(unsafe.Pointer(&data[:1][0])))
+	}
+	return data
 }
 
 func (p *pcache) Free(data []byte) {
 	capacity := cap(data)
+	if capacity == 0 {
+		return
+	}
 	cidx := calcCapIndex(capacity)
+	clen := 1 << cidx
+	dp := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+	addr := dp.Data
 	data = data[:0]
+	dp.Cap = clen
 
 	pid := procPin()
-	// if out of active limit, put into inactive
-	if p.activeLimit > 0 && p.activeSize[pid] >= p.activeLimit {
-		p.inactive[pid][cidx] = append(p.inactive[pid][cidx], data)
-	} else {
-		p.activeSize[pid] += int64(capacity)
+	if addr >= p.arenaStart && addr <= p.arenaEnd {
 		p.active[pid][cidx] = append(p.active[pid][cidx], data)
+		procUnpin()
+		if debug {
+			log.Printf("PCACHE: P[%d] free active %d bytes, addr %d", pid, clen, addr)
+		}
+		return
 	}
+
+	p.inactive[pid][cidx] = append(p.inactive[pid][cidx], data)
 	procUnpin()
+	if debug {
+		log.Printf("PCACHE: P[%d] free inactive %d bytes, addr %d", pid, clen, addr)
+	}
 }
 
 func calcCapIndex(size int) int {
--- pcache_test.go
+++ pcache_test.go
@@ -1,11 +1,17 @@
+//+build !race
+
 package netpoll
 
 import (
+	"fmt"
 	"runtime"
+	"sync"
 	"testing"
+
+	"github.com/bytedance/gopkg/lang/mcache"
 )
 
-func TestPCache(t *testing.T) {
+func TestPCacheSingleP(t *testing.T) {
 	procs := runtime.GOMAXPROCS(0)
 	runtime.GOMAXPROCS(1)
 	defer runtime.GOMAXPROCS(procs)
@@ -14,7 +20,7 @@ func TestPCache(t *testing.T) {
 	pc := newPCache()
 	buf := pc.Malloc(1024, 1025)
 	Equal(t, len(buf), 1024)
-	Equal(t, cap(buf), 2048)
+	Equal(t, cap(buf), 1025)
 	pc.Free(buf)
 	Equal(t, len(pc.active[0][calcCapIndex(1025)]), 1)
 
@@ -22,8 +28,35 @@ func TestPCache(t *testing.T) {
 	Equal(t, len(buf), 1024)
 	Equal(t, cap(buf), 1024)
 	pc.Free(buf)
-	Equal(t, len(pc.active[0][calcCapIndex(1025)]), 1)
 	Equal(t, len(pc.active[0][calcCapIndex(1024)]), 1)
+	Equal(t, len(pc.active[0][calcCapIndex(1025)]), 1)
+}
+
+func TestPCacheMultiP(t *testing.T) {
+	pc := newLimitedPCache(1024) // 1KB
+	size := 50
+	testdata := make([]byte, size)
+	for i := 0; i < len(testdata); i++ {
+		testdata[i] = 'a' + byte(i%26)
+	}
+	procs := runtime.GOMAXPROCS(0)
+	var wg sync.WaitGroup
+	for i := 0; i < procs; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				buf := pc.Malloc(size, size)
+				copy(buf, testdata)
+				Equal(t, len(buf), size)
+				Equal(t, cap(buf), size)
+				Equal(t, string(buf), string(testdata))
+				runtime.Gosched()
+				pc.Free(buf)
+			}
+		}()
+	}
+	wg.Wait()
 }
 
 func TestPCacheGC(t *testing.T) {
@@ -60,4 +93,46 @@ func TestPCacheGC(t *testing.T) {
 	}
 	Equal(t, len(pc.active[0][calcCapIndex(1024)]), 1)
 	Equal(t, len(pc.inactive[0][calcCapIndex(1024)]), 0)
+}
+
+var benchSizes = []int{
+	1024,
+	1024 * 8, 1024 * 64, // small size
+	1024 * 1024, 1024 * 1024 * 8, // large size
+} // unit: Bytes
+
+func BenchmarkPCache(b *testing.B) {
+	for _, size := range benchSizes {
+		pc := newLimitedPCache(1024 * 1024 * 1) // 1MB
+		b.Run(fmt.Sprintf("Malloc-%d", size), func(b *testing.B) {
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					buf := pc.Malloc(size)
+					pc.Free(buf)
+				}
+			})
+		})
+		d := make([]int, len(pc.active))
+		for pid := 0; pid < len(pc.active); pid++ {
+			for cidx := 0; cidx < len(pc.active[pid]); cidx++ {
+				for i := 0; i < len(pc.active[pid][cidx]); i++ {
+					d[pid] += cap(pc.active[pid][cidx][i])
+				}
+			}
+		}
+		b.Logf("pcache-%d active distribution: %v", size, d)
+	}
+}
+
+func BenchmarkMCache(b *testing.B) {
+	for _, size := range benchSizes {
+		b.Run(fmt.Sprintf("Malloc-%d", size), func(b *testing.B) {
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					buf := mcache.Malloc(size)
+					mcache.Free(buf)
+				}
+			})
+		})
+	}
 }
